741d7365cf12f2bdd61586674ac5667eebcea897
smilepyt <307080368@qq.com>
2018-07-17 21:19:00 +0800

    添加部分注释信息

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,11 +1,13 @@
 import axios from 'axios'
 
 export default {
+  //初始化数据，通过axios和mock.js获取模拟数据
   initializeData({commit}) {
     axios.get('/getTopics').then(function (response) {
       commit('INITIALIZE_DATA', response.data);
     })
   },
+  //点击下一题，记录答案id,判断是否为最后一题，如果不是题号+1
   addNum({commit,state}, id) {
     commit('REMEMBER_ANSWER', id);
     if (state.itemNum < state.itemDetail.length) {
--- src/store/mutations.js
+++ src/store/mutations.js
@@ -3,19 +3,20 @@ const ADD_ITEMNUM = 'ADD_ITEMNUM';
 const REMEMBER_ANSWER = 'REMEMBER_ANSWER';
 
 export default {
+  //初始化数据
   [INITIALIZE_DATA](state, data) {
-    state.itemNum = 1;
-    state.allTime = 0;
-    state.answerid = [];
+    state.itemNum = 1;    //当前题号
+    state.allTime = 0;    //初始时间
+    state.answerid = [];  //答案数组
 
     state.level = data.level;
     state.itemDetail = data.itemDetail;
   },
-
+  //记录答案，答案id唯一
   [REMEMBER_ANSWER](state, id) {
     state.answerid.push(id);
   },
-
+  //点击进入下一题
   [ADD_ITEMNUM](state, num) {
     state.itemNum += num;
   }
